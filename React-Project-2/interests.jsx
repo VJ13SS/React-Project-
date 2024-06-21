3f59c30aa65e4f8d18b1db182ac30f964be2e466
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-06-21 21:50:40 +0530

    Create interests.jsx

---
+++ React-Project-2/interests.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function Interests(){
+  return(
+    <div className = "interests">
+      <h5 className = "interests-header">Interests</h5>
+      <p className = "intersests-text">Passionate about leveraging AI, blockchain, IoT, and data analytics to drive innovation and business transformation.</p>
+    </div>
+  )
+}
