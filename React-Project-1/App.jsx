39e31ac26f7cdca867afe5c3e764a6b0a07d9806
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-06-21 09:32:19 +0530

    Create App.jsx

---
+++ React-Project-1/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './components/Nav.jsx'
+import Main from './components/main.jsx'
+import './App.css'
+export default function App(){
+  return (
+    <div>
+      <Nav />
+      <Main />
+    </div>
+  )
+}
