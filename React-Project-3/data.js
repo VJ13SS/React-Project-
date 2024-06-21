43878243cebdc90d5e4b7aedde79cd84edf14ec9
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-06-21 22:10:28 +0530

    Create data.js

---
+++ React-Project-3/data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    img: "src/components/Images/Card3.jpeg",
+    header:"Velhe,Torna-Rajgad, India",
+    text : "Mountain and Poll views",
+    reviews : 4.9,
+    price : 10540,
+    date : "21-26 Jul",
+    trending: 1
+  },
+  {
+    img: "src/components/Images/Card2.jpeg",
+    header:"Kamshet, India",
+    text : "Mountain and Garden views",
+    reviews : 4.86,
+    price : 5709,
+    date : "19-24 Jun",
+    trending: 0
+  },
+  {
+    img: "src/components/Images/Card1.jpeg",
+    header:"Velhe,Torna-Rajgad, India",
+    text : "74 kilometres away",
+    reviews : 4.7,
+    price : 8580,
+    date : "30 Jun-5 Jul",
+    trending: 0
+  }
+]
+
+export default data;
