c3719be6d5ea4ff18893e5386e54124d22e4225f
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-06-21 09:34:31 +0530

    Create main.jsx

---
+++ React-Project-1/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+export default function Main(){
+  return (
+    <main>
+      <h1 className = "main-header">Fun Facts About React</h1>
+      <ul className = "main-facts">
+        <li>First released in 2013.</li>
+        <li>Created by Jordan Walke, a software engineer at Facebook.</li>     
+        <li>It was developed and is now maintained by Facebook</li>
+        <li>The React library is open-source and has a large community of developers.</li>
+        <li>React's primary purpose is to handle the view layer of web applications.</li>
+      </ul>
+    </main>
+  )
+}
