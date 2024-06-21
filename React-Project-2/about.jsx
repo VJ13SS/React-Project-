bfe83d84eb47a5b8303bdecf1daa601a70295807
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-06-21 21:48:15 +0530

    Create about.jsx

---
+++ React-Project-2/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function About(){
+  return(
+    <div className = "about">
+      <h5 className = "about-header">About</h5>
+      <p className = "about-text">Empowering businesses with cutting-edge solutions in AI, blockchain, IoT, and data analytics for sustainable growth and innovation.</p>
+    </div>
+  )
+}
