6519d4b4eac053b60e1422ec7c4ba20d7002f555
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-06-21 22:11:48 +0530

    Create nav.jsx

---
+++ React-Project-3/nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from './Images/Logo.png'
+
+export default function Nav(){
+  return (
+    <nav>
+      <img src = {Logo} className = "nav-img"/>
+    </nav>
+  )
+}
