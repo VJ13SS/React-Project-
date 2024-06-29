63f45487a7f29910e5eef96028ed6eee9cbfdd59
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-06-29 09:41:14 +0530

    Create header.jsx

---
+++ React-Project-5/components/header.jsx
@@ -0,0 +1,10 @@
+import React from'react'
+
+export default function Header(){
+  return(
+    <header className = "header">
+      <h2 className = "header-title">Meme Generator</h2>
+      <h4 className = "header-text">React Course - Project 5</h4>
+    </header>
+  )
+}
